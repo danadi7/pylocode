b47011ff6079a787ecf17e440ab82c4278c83b91
Daniel <daniel@adip.me>
2022-09-30 03:24:52 +0800

    Add input cleanup code

--- pylocode.py
+++ pylocode.py
@@ -1,6 +1,11 @@
 from countries import countries
 from locodes import locodes
 
+def _clean(code):
+    if len(code) > 6 or (len(code) == 6 and code[2] != " "):
+        raise ValueError("Not a valid country code")
+    return code.upper()
+
 def _extract_country_code(code):
     return code[:2]
 
@@ -8,6 +13,7 @@ def _extract_location_code(code):
     return code[-3:]
 
 def get_name_by_locode(code):
+    code = _clean(code)
     country = _extract_country_code(code)
     port_code = _extract_location_code(code)
     name = locodes.get(country).get(port_code)
@@ -16,12 +22,14 @@ def get_name_by_locode(code):
     return name
 
 def get_country_name_by_country_code(code):
+    code = _clean(code)
     name = countries.get(code)
     if not name:
         raise ValueError("Not a valid country code")
     return name
 
 def get_country_name_by_locode(code):
+    code = _clean(code)
     country = _extract_country_code(code)
     name = countries.get(country)
     if not name:
